bf8a1e024637ea503e7017e973a811f7c2d0e773
nilesh <nilesh.chaudhari@paysense.in>
2020-12-24 20:24:06 +0530

    create config

---
+++ config.py
@@ -0,0 +1,21 @@
+"""ALL the config needed for this experiment."""
+
+
+class Config:
+    # numeric constants
+    MIN_SAMPLES_PER_INTENT = 50
+    TRAIN_SPLIT = 0.7
+
+    # filepaths
+    RAW_DATA_FILEPATH = "./data/training_data_for_query_classification.pkl"
+    TRAINING_DATA_FILEPATH = "./data/training_data.json"
+    TESTING_DATA_FILEPATH = "./data/testing_data.json"
+
+    #
+    LOG_FORMAT = (
+        "%(asctime)s %(levelname)-2s [%(filename)s:%(lineno)d] %(message)s"
+    )
+    DATE_FORMAT = "%Y-%m-%d:%H:%M:%S"
+
+
+config = Config()
