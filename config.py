de123fee98ff5cbc4914f09b8335661f53b42ae8
nilesh <nilesh.chaudhari@paysense.in>
2020-12-25 12:25:15 +0530

    update config

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     MIN_SAMPLES_PER_INTENT = 50
     TRAIN_SPLIT = 0.7
     MIN_NGRAM_COUNT = 16
+    HIDDEN_DIM = 256
 
     # filepaths
     RAW_DATA_FILEPATH = "./data/training_data_for_query_classification.pkl"
@@ -13,6 +14,7 @@ class Config:
     TESTING_DATA_FILEPATH = "./data/testing_data.json"
     FEATURE_MAP_FILEPATH = "./data/feature_map.json"
     LABEL_MAP_FILEPATH = "./data/label_map.json"
+    INIT_MODEL_PATH = "./init_model.pt"
 
     #
     LOG_FORMAT = "%(asctime)s %(levelname)-2s [%(filename)s:%(lineno)d] %(message)s"
