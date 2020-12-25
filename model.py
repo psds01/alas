4e5f6c495548c986032d91e32c83d4467a44e900
nilesh <nilesh.chaudhari@paysense.in>
2020-12-25 12:54:34 +0530

    add logging

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import typing
 from typing import Any, Dict, List, Union
@@ -6,6 +7,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+logger = logging.getLogger(__name__)
 
 
 class NNModel(nn.Module):
@@ -25,9 +28,13 @@ def get_net(n_features: int, n_hidden: int, n_classes: int, init_filepath: os.Pa
     Utility to define and INITIALIZE the model
     with the same weights for different runs.
     """
+    logger.info("Defining model.")
     net = NNModel(n_features, n_hidden, n_classes)
     # if path does not exist, save and load (redundant!)
     if not os.path.exists(init_filepath):
+        logger.info("Saved weights do not exist, saving model weights.")
         torch.save(net.state_dict(), init_filepath)
+    logger.info("Initializing model with saved weights.")
     net.load_state_dict(torch.load(init_filepath))
+    logger.info("Done.")
     return net
