bb4c584f830a635edba87f08f01b22e5fb8d32ed
nilesh <nilesh.chaudhari@paysense.in>
2020-12-25 12:01:03 +0530

    USE PYTORCH!!!

--- model.py
+++ model.py
@@ -2,8 +2,10 @@ import typing
 from typing import Any, Dict, List, Union
 
 import numpy as np
-import tensorflow as tf
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-class NNModel:
+class NNModel(nn.Module):
     pass
